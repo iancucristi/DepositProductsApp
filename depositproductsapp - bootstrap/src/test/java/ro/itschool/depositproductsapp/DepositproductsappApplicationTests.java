2c08c701d06bca2d32a1dcd7a6eed7a7e7dbeb5b
iancucristi <krystyke93@gmail.com>
2022-06-05 21:06:24 +0300

    no message

---
+++ depositproductsapp - bootstrap/src/test/java/ro/itschool/depositproductsapp/DepositproductsappApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.itschool.depositproductsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositproductsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
